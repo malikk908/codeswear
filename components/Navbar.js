eedf43bc929b236eb3b13d344a793fbd20d5f26a
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-08-12 13:44:25 +0500

    feat - Cart State

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ import {BsFillBagCheckFill} from 'react-icons/bs';
 import { useRef } from 'react';
 
 
-const Navbar = () => {
+const Navbar = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
 
   const ref = useRef()
 
@@ -23,7 +23,7 @@ const Navbar = () => {
 
 
   return (
-    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md'>
+    <div className='flex flex-col md:flex-row md:justify-start justify-center items-center py-2 shadow-md sticky top-0 z-10 bg-white'>
       <div className="logo mx-5">
         <Link href={"/"}><img src="/logo2.png" alt="" /></Link>
       </div>
@@ -38,59 +38,27 @@ const Navbar = () => {
       <div onClick={toggleCart} className="cart absolute right-0 top-2 mx-5 font-bold">
         <button><AiOutlineShoppingCart className='text-xl md:text-3xl' /></button>
       </div>
-      <div ref={ref} className='w-72 z-10 h-full sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform translate-x-full'>
+      <div ref={ref} className='w-72 z-10 h-[100vh] sideCart absolute top-0 right-0 bg-pink-100 px-8 py-10 transform transition-transform translate-x-full'>
         <h2 className='font-bold text-l text-center'>Shopping Cart</h2>
         <span className='absolute top-3 right-3 cursor-pointer text-pink-500 text-2xl' onClick={toggleCart}><AiFillCloseCircle /></span>
-        <ol className='list-decimal'>
-          <li className='my-3'>
+        <ol className='list-decimal font-semibold'>
+          {Object.keys(cart).length ==0 && <div className='my-4'>Your cart is Empty!</div>}
+          {Object.keys(cart).map((k)=>{
+            return <li key={k} className='my-3'>
             <div className="item flex">
-              <div className='font-semibold w-2/3'>Tshirt - Wear the code</div>
+              <div className='font-semibold w-2/3'>{cart[k].name}</div>
               <div className='flex items-center justify-center w-1/3 text-lg'>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500'/><span className='mx-2'> 1 </span><AiFillPlusCircle className='cursor-pointer text-pink-500'/></div>
+                <AiFillMinusCircle onClick={()=>{removeFromCart(k, 1)}} className='cursor-pointer text-pink-500'/><span className='mx-2'> {cart[k].qty} </span><AiFillPlusCircle onClick={()=>{addToCart(k, 1)}} className='cursor-pointer text-pink-500'/></div>
             </div>
-          </li>
-          <li className='my-3'>
-            <div className="item flex">
-              <div className='font-semibold w-2/3'>Tshirt - Wear the code</div>
-              <div className='flex items-center justify-center w-1/3 text-lg'>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500'/><span className='mx-2'> 1 </span><AiFillPlusCircle className='cursor-pointer text-pink-500'/></div>
-            </div>
-          </li>
-          <li className='my-3'>
-            <div className="item flex">
-              <div className='font-semibold w-2/3'>Tshirt - Wear the code</div>
-              <div className='flex items-center justify-center w-1/3 text-lg'>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500'/><span className='mx-2'> 1 </span><AiFillPlusCircle className='cursor-pointer text-pink-500'/></div>
-            </div>
-          </li>
-          <li className='my-3'>
-            <div className="item flex">
-              <div className='font-semibold w-2/3'>Tshirt - Wear the code</div>
-              <div className='flex items-center justify-center w-1/3 text-lg'>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500'/><span className='mx-2'> 1 </span><AiFillPlusCircle className='cursor-pointer text-pink-500'/></div>
-            </div>
-          </li>
-          <li className='my-3'>
-            <div className="item flex">
-              <div className='font-semibold w-2/3'>Tshirt - Wear the code</div>
-              <div className='flex items-center justify-center w-1/3 text-lg'>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500'/><span className='mx-2'> 1 </span><AiFillPlusCircle className='cursor-pointer text-pink-500'/></div>
-            </div>
-          </li>
-          <li className='my-3'>
-            <div className="item flex">
-              <div className='font-semibold w-2/3'>Tshirt - Wear the code</div>
-              <div className='flex items-center justify-center w-1/3 text-lg'>
-                <AiFillMinusCircle className='cursor-pointer text-pink-500'/><span className='mx-2'> 1 </span><AiFillPlusCircle className='cursor-pointer text-pink-500'/></div>
-            </div>
-          </li>
+          </li>})}
+          
           
           
         </ol>
         <div className="flex">
 
-        <button class="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm"> <BsFillBagCheckFill className='m-1 mr-3'/> Checkout</button>
-        <button class="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">Clear Cart</button>
+        <button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm"> <BsFillBagCheckFill className='m-1 mr-3'/> Checkout</button>
+        <button onClick={clearCart} className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">Clear Cart</button>
 
         </div>
         
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,70 @@ import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import '@/styles/globals.css'
 import Head from 'next/head'
+import { useState, useEffect } from 'react'
 
 export default function App({ Component, pageProps }) {
+
+  const [cart, setCart] = useState({})
+  const[subTotal, setSubTotal] = useState(0)
+
+  useEffect(()=>{
+    console.log('Heyyy')
+    try {
+      if(localStorage.getItem("cart")){
+        setCart(JSON.parse(localStorage.getItem("cart")))
+      }      
+    } catch (error) {
+      console.error(error)
+      localStorage.clear()
+    }
+    
+  }, [])
+
+  const saveCart = (myCart)=>{
+    localStorage.setItem("cart", JSON.stringify(myCart))
+    let subt = 0;
+    let keys = Object.keys(myCart)
+    for (let i = 0; i<keys.length; i++) {
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty;
+           
+    }
+    setSubTotal(subt)
+  }
+
+  const addToCart = (itemCode, qty, price, name, size, variant)=>{
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty = newCart[itemCode].qty + qty
+
+    }else{
+      newCart[itemCode] = {qty:1, price, name, size, variant}
+    }
+    setCart(newCart)
+    saveCart(newCart)
+    
+  }
+
+  const removeFromCart = (itemCode, qty)=>{
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty = newCart[itemCode].qty - qty
+
+    }if(newCart[itemCode].qty <= 0){
+      delete newCart[itemCode]
+    }
+    setCart(newCart)
+    saveCart(newCart)
+    
+  }
+
+  const clearCart = ()=>{
+    setCart({})
+    saveCart({})
+  }
+
+
+
   return <>
 
   <Head>
@@ -11,9 +73,9 @@ export default function App({ Component, pageProps }) {
   </Head>
 
 
-  <Navbar/>
+  <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} />
   
-  <Component {...pageProps} />
+  <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
   
   <Footer/>
 
