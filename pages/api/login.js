6b8bf9cac12961c2151129b8bcdb79f64b80eea7
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-08-17 19:05:09 +0500

    feat - password encryption using crypto-js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,23 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import User from "@/models/User"
 import connectDb from "@/middleware/mongoose"
+var CryptoJS = require("crypto-js");
+
 
 const handler = async (req, res) => {
     if (req.method == 'POST') {
         console.log(req.body)
-        let user = await User.findOne({email: req.body.email})
-        if(user){
-            if(req.body.email == user.email && req.body.password == user.password){
+        let user = await User.findOne({ email: req.body.email })
+        if (user) {
+            let encryptedPass = CryptoJS.AES.decrypt(user.password, 'secret123');
+            let originalPass = encryptedPass.toString(CryptoJS.enc.Utf8);
+
+            if (req.body.email == user.email && req.body.password == originalPass) {
                 res.status(200).json({ success: true, name: user.name, email: user.email })
 
-            }else{
+            } else {
                 res.status(200).json({ success: false, error: 'Invalid Credentials' })
 
             }
 
-            
-        }else{
-            res.status(200).json({ success: false, error: 'No user found' })
+
+        } else {
+            res.status(200).json({ success: false, error: 'User Not Found' })
         }
 
 
