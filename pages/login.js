6520de5b7f869d4106cfb18144f0af8255c558cd
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-08-17 16:01:52 +0500

    feat - login/signup

--- pages/login.js
+++ pages/login.js
@@ -1,33 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/router';
 
 
 const login = () => {
+
+  const router = useRouter()
+
+
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault()
+
+    const data = { email, password }
+    console.log(data)
+    let res = await fetch('http://localhost:3000/api/login', {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+
+    let response = await res.json()
+    console.log(response)  
+
+    if(response.success){
+      toast.success('Your have successfully logged in!')
+      setEmail('')
+      setPassword('')
+      setTimeout(() => {
+        router.push('http://localhost:3000')
+      }, 1500);
+
+    }else{
+      toast.error(response.error)
+
+    }
+
+  }
+  
+
+  const handleChange = (e) => {
+
+    if (e.target.name == 'email') {
+      setEmail(e.target.value)
+
+    }
+    else if (e.target.name == 'password') {
+      setPassword(e.target.value)
+    }
+  }
+
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img className="mx-auto h-20 w-auto" src="/logo1.png" alt="Your Company" />
         <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form onSubmit={handleSubmit} className="space-y-6" method="POST">
           <div>
-            <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div className="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+              <input value={email} onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
           <div>
             <div className="flex items-center justify-between">
-              <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
               <div className="text-sm">
                 <Link href="/forgot" className="font-semibold text-pink-600 hover:text-pink-500">Forgot password?</Link>
               </div>
             </div>
             <div className="mt-2">
-              <input id="password" name="password" type="password" autocomplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+              <input value={password} onChange={handleChange} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
--- pages/signup.js
+++ pages/signup.js
@@ -1,36 +1,99 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const signup = () => {
+
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = { name, email, password }
+    console.log(data)
+    let res = await fetch('http://localhost:3000/api/signup', {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+
+    let response = await res.json()
+    console.log(response)
+
+    if (response.success) {
+      toast.success('Your account has been created!')
+      setName('')
+      setEmail('')
+      setPassword('')
+
+    } else {
+      toast.error(response.error)
+    }
+  }
+
+  const handleChange = (e) => {
+    if (e.target.name == 'name') {
+      setName(e.target.value)
+    }
+    else if (e.target.name == 'email') {
+      setEmail(e.target.value)
+
+    }
+    else if (e.target.name == 'password') {
+      setPassword(e.target.value)
+    }
+  }
+
+
+
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img className="mx-auto h-20 w-auto" src="/logo1.png" alt="Your Company" />
         <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up for an account</h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-        <div>
-            <label for="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
+        <form onSubmit={handleSubmit} className="space-y-6" method="POST">
+          <div>
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">Name</label>
             <div className="mt-2">
-              <input id="name" name="name" type="text" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+              <input value={name} onChange={handleChange} id="name" name="name" type="text" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
           <div>
-            <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div className="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+              <input value={email} onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
           <div>
             <div className="flex items-center justify-between">
-              <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
 
             </div>
             <div className="mt-2">
-              <input id="password" name="password" type="password" autocomplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+              <input value={password} onChange={handleChange} id="password" name="password" type="password" autoComplete="current-password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
             </div>
           </div>
 
