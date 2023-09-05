c596502e079e2c63c6eabd47e3797eb81c530d5b
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-09-05 18:38:25 +0500

    Forgot Password funcionality

---
+++ pages/api/forgot.js
@@ -0,0 +1,81 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import Forgot from "@/models/Forgot"
+import User from "@/models/User"
+import connectDb from "@/middleware/mongoose"
+var CryptoJS = require("crypto-js");
+
+const handler = async (req, res) => {
+
+    //Check if the user exists in database
+
+    //send email to the user
+
+    if (req.body.sendMail) {
+
+        let token = `123`
+
+        let forgot = new Forgot({
+            email: req.body.email,
+            token: token
+        })
+        await forgot.save()
+
+        let email = `We have sent you this email in response to your request to reset your password on Codeswear/com.
+
+  <br/><br/>
+
+  To reset your password, please follow the link below:
+
+  <a href="https://codeswear.com/forgot?token=${token}">Click here to reset your password</a>
+
+  <br/><br/>
+
+  We recommend that you keep your password secure and not share it with anyone.If you feel your password has been compromised, you can change it by going to your My Account Page.
+
+  <br/><br/>`
+
+        res.status(200).json({ success: true, token })
+
+
+    } else {
+        //Reset password
+
+        try {
+
+            // let email = req.body.email
+            let password = req.body.password
+            let token = req.body.token
+
+            try {
+                let forgot = await Forgot.findOne({ token: token })
+
+                await User.findOneAndUpdate({ email: forgot.email }, { password: CryptoJS.AES.encrypt(password, 'secret123').toString() })
+
+                res.status(200).json({ success: true, message: 'Password updated successfully' })
+
+            } catch (error) {
+                res.status(400).json({ success: false, message: 'Session Expired, try again' })
+
+            }
+
+
+
+
+
+
+
+
+
+
+        } catch (error) {
+            console.log(error)
+            res.status(400).json({ error })
+        }
+
+    }
+
+}
+
+export default connectDb(handler)
+
--- pages/forgot.js
+++ pages/forgot.js
@@ -1,6 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import axios from 'axios'
+import { toast } from 'react-toastify';
+
 
 
 const forgot = () => {
@@ -8,10 +11,96 @@ const forgot = () => {
   const router = useRouter()
 
   useEffect(() => {
-    if(localStorage.getItem('token')){
+    if (localStorage.getItem('token')) {
       router.push('/')
     }
   }, [])
+
+  useEffect(()=>{
+    if(!router.isReady) return;
+
+    // codes using router.query
+    console.log(router.query)
+
+}, [router.isReady]);
+
+
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [cpassword, setCpassword] = useState('')
+
+
+  const handleChange = async (e) => {
+    if (e.target.name == 'email') {
+      setEmail(e.target.value)
+    }
+    else if (e.target.name == 'password') {
+      setPassword(e.target.value)
+    }
+    else if (e.target.name == 'cpassword') {
+      setCpassword(e.target.value)
+    }
+  }
+
+  const sendResetEmail = async (e) => {
+
+    e.preventDefault()
+
+    try {
+      const { data } = await axios.post(
+        `/api/forgot`,
+        {
+          email,
+          sendMail: true
+        }
+      );
+
+      router.push(`/forgot?token=${data.token}`)
+      toast.success('Password Reset instructions have been sent to your email')
+
+    } catch (error) {
+      console.log(error)
+      toast.error('Some error occured')
+    }
+
+  }
+
+  const resetPassword = async (e) => {
+
+    e.preventDefault()
+
+    if (password === cpassword) {
+
+      try {
+        let token = router.query.token
+        console.log(token)
+        const { data } = await axios.post(
+          `/api/forgot`,
+          {
+            email,
+            password,
+            token,
+            sendMail: false
+          }
+        );
+        router.push(`/login`)
+        toast.success(data.message)
+
+      } catch (error) {
+        console.log(error)
+        toast.error(error.message)
+      }
+
+
+    }
+    else {
+      toast.error('Password not matched, try again')
+
+    }
+
+  }
+
 
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -22,18 +111,38 @@ const forgot = () => {
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form className="space-y-6" action="#" method="POST">
-          <div>
-            <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+          {!router.query.token && <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
             <div className="mt-2">
-              <input id="email" name="email" type="email" autocomplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+              <input value={email} onChange={handleChange} id="email" name="email" type="email" autoComplete="email" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
             </div>
+            <div className='mt-4'>
+              <button onClick={sendResetEmail} type="submit" className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Continue</button>
+            </div>
+          </div>}
+          {router.query.token && <div>
+            <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">New Password</label>
+            <div className="mt-2">
+              <input value={password} onChange={handleChange} id="password" name="password" type="password" autoComplete="password" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+            </div>
+            <label htmlFor="cpassword" className="mt-4 block text-sm font-medium leading-6 text-gray-900">Confirm Password</label>
+            <div className="mt-2">
+              <input value={cpassword} onChange={handleChange} id="cpassword" name="cpassword" type="password" autoComplete="cpassword" required className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-pink-600 sm:text-sm sm:leading-6" />
+            </div>
+            <div className='mt-4'>
+              <button disabled={password !== cpassword} onClick={resetPassword} type="submit" className={`flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm ${password === cpassword ? `hover:bg-pink-600` : 'disabled:opacity-50'} focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600`}>Reset Password</button>
+            </div>
+            {cpassword && password != cpassword &&
+              <span className='text-red-600'>Password Not Matched</span>}
+            {password && password == cpassword &&
+              <span className='text-green-600'>Password Matched</span>}
           </div>
 
- 
+          }
 
-          <div>
-            <button type="submit" className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Continue</button>
-          </div>
+
+
+
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
