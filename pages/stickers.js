c35b426f35412292e3aca3f5f15b86883e62bf2a
malikk908 <136706872+malikk908@users.noreply.github.com>
2023-10-08 16:23:33 +0500

    product page css fix

--- pages/stickers.js
+++ pages/stickers.js
@@ -8,6 +8,7 @@ const Stickers = () => {
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
+
             <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
               <Link href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
                 <img alt="ecommerce" className="h-[36vh] md:h-[42vh] block m-auto" src="https://m.media-amazon.com/images/I/71A-czu4m1L._AC_UX679_.jpg" />
@@ -19,6 +20,7 @@ const Stickers = () => {
                   <p className="mt-1">Rs 1099</p>
                 </div></Link>
             </div>
+            
             <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
               <Link href={'/product/wear-the-code'} className="block relative rounded overflow-hidden">
                 <img alt="ecommerce" className="h-[36vh] md:h-[42vh] block m-auto" src="https://m.media-amazon.com/images/I/71A-czu4m1L._AC_UX679_.jpg" />
--- pages/tshirts.js
+++ pages/tshirts.js
@@ -9,38 +9,41 @@ const Tshirts = ({ products }) => {
 
   return (
     <div>
+      
       <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-wrap -m-4">
 
             {Object.keys(products).map((item) => {
 
-              return <Link key={products[item]._id} passHref={true} href={`/product/${products[item].slug}`} >
-                <div className="lg:w-auto md:w-1/2 p-8 mx-5 w-full shadow-lg">
-                  <div className="block relative rounded overflow-hidden">
-                    <img alt="ecommerce" className="h-[36vh] md:h-[42vh] block m-auto" src={products[item].img} />
-                  </div>
-                  <div className="mt-4 text-center md:text-left">
-                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{products[item].category}</h3>
-                    <h2 className="text-gray-900 title-font text-lg font-medium">{products[item].title}</h2>
-                    <p className="mt-1">Rs {products[item].price}</p>
-                    <div className="mt-1">
-                      {products[item].size.includes('S') && <span className='border border-gray-300 px-1 mx-1'>S</span>}
-                      {products[item].size.includes('M') && <span className='border border-gray-300 px-1 mx-1'>M</span>}
-                      {products[item].size.includes('L') && <span className='border border-gray-300 px-1 mx-1'>L</span>}
-                      {products[item].size.includes('XL') && <span className='border border-gray-300 px-1 mx-1'>XL</span>}
-                      {products[item].size.includes('XXL') && <span className='border border-gray-300 px-1 mx-1'>XXL</span>}
+              return (
+                <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-lg">
+                  <Link key={products[item]._id} passHref={true} href={`/product/${products[item].slug}`} >
+                    <div className="block relative rounded overflow-hidden">
+                      <img alt="ecommerce" className="h-[36vh] md:h-[42vh] block m-auto" src={products[item].img} />
                     </div>
-                    <div className="mt-1">
-                      {products[item].color.includes('blue') && <button className='border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none'></button>}
-                      {products[item].color.includes('red') && <button className='border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none'></button>}
-                      {products[item].color.includes('yellow') && <button className='border-2 border-gray-300 ml-1 bg-yellow-500 rounded-full w-6 h-6 focus:outline-none'></button>}
-                      {products[item].color.includes('purple') && <button className='border-2 border-gray-300 ml-1 bg-purple-500 rounded-full w-6 h-6 focus:outline-none'></button>}
-                      {products[item].color.includes('green') && <button className='border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none'></button>}
+                    <div className="mt-4 text-center md:text-left">
+                      <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{products[item].category}</h3>
+                      <h2 className="text-gray-900 title-font text-lg font-medium">{products[item].title}</h2>
+                      <p className="mt-1">Rs {products[item].price}</p>
+                      <div className="mt-1">
+                        {products[item].size.includes('S') && <span className='border border-gray-300 px-1 mx-1'>S</span>}
+                        {products[item].size.includes('M') && <span className='border border-gray-300 px-1 mx-1'>M</span>}
+                        {products[item].size.includes('L') && <span className='border border-gray-300 px-1 mx-1'>L</span>}
+                        {products[item].size.includes('XL') && <span className='border border-gray-300 px-1 mx-1'>XL</span>}
+                        {products[item].size.includes('XXL') && <span className='border border-gray-300 px-1 mx-1'>XXL</span>}
+                      </div>
+                      <div className="mt-1">
+                        {products[item].color.includes('blue') && <button className='border-2 border-gray-300 ml-1 bg-blue-500 rounded-full w-6 h-6 focus:outline-none'></button>}
+                        {products[item].color.includes('red') && <button className='border-2 border-gray-300 ml-1 bg-red-500 rounded-full w-6 h-6 focus:outline-none'></button>}
+                        {products[item].color.includes('yellow') && <button className='border-2 border-gray-300 ml-1 bg-yellow-500 rounded-full w-6 h-6 focus:outline-none'></button>}
+                        {products[item].color.includes('purple') && <button className='border-2 border-gray-300 ml-1 bg-purple-500 rounded-full w-6 h-6 focus:outline-none'></button>}
+                        {products[item].color.includes('green') && <button className='border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none'></button>}
+                      </div>
                     </div>
-                  </div>
+                  </Link>
                 </div>
-              </Link>
+              )
             })}
           </div>
         </div>
